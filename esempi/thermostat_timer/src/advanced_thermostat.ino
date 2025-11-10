04b7c36c7f4429a47d77f3b5efbbdea2d305c745
Riccardo Bacca <riccardo.bacca@studio.unibo.it>
2025-11-10 09:12:41 +0100

    Updates

--- esempi/thermostat_timer/src/advanced_thermostat.ino
+++ esempi/thermostat_timer/src/advanced_thermostat.ino
@@ -2,20 +2,38 @@
 #include "setup.h"
 #include "Temp.h"
 #include "logic.h"
-
-
+ 
+#define PIR_PORT 2
+ 
+bool isMoving = false;
+ 
 void setup() {
     Serial.begin(115200);
-
+ 
+    pinMode(PIR_PORT, INPUT);
     pinSetup();
     initTimer();
     resetThermostat();
 }
-
+ 
+void waiting() {
+    if (timerFlag) {
+        // CAMBIO DI STATO
+        if (currentState == COMFORT_STANDBY) {
+            currentState = ECO_STANDBY;
+        } else if (currentState == COMFORT_HEATING) {
+            currentState = ECO_HEATING;
+        }
+        timerFlag = false;
+    }
+}
+ 
 void loop() {
     float currentTemp = readCurrentTemperature();
-    changeEcoComfort();
-
+ 
+    isMoving = digitalRead(PIR_PORT);
+    waiting();
+ 
     switch (currentState) {
         case COMFORT_STANDBY:
             if (currentTemp < comfortSetPoint) {
@@ -23,27 +41,44 @@ void loop() {
                 Serial.println("COMFORT_HEATING");
             }
             break;
-
+ 
         case COMFORT_HEATING:
             if (currentTemp >= comfortSetPoint) {
                 currentState = COMFORT_STANDBY;
                 Serial.println("→ COMFORT_STANDBY");
             }
+ 
+            if(!isMoving) {
+                currentState = ECO_HEATING;
+                waiting();
+            }
             break;
-
+ 
         case ECO_STANDBY:
             if (currentTemp < ecoSetPoint) {
                 currentState = ECO_HEATING;
                 Serial.println("→ ECO_HEATING");
             }
+ 
+            if(isMoving) {
+                currentState = COMFORT_STANDBY;
+                waiting();
+            }
             break;
-
+ 
         case ECO_HEATING:
             if (currentTemp >= ecoSetPoint) {
                 currentState = ECO_STANDBY;
                 Serial.println("→ ECO_STANDBY");
             }
+ 
+            if(isMoving) {
+                currentState = COMFORT_HEATING;
+                waiting();
+            }
             break;
     }
-
+ 
 }
+ 
+ 
